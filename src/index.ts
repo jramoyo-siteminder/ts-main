352b72a7b467d7638cfc492d3ce53bff8cfc59e0
Jan Amoyo <jan.amoyo@siteminder.com>
2017-02-27 11:26:37 +1100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Person } from 'ts-npm-module';
+import { Place } from 'ts-npm-module';
+
+let jan = new Person('Jan', 'Amoyo');
+let work = new Place('work', '88 Cumberland St, The Rocks NSW 2000');
+
+console.log(`Name: ${jan.getFullName()}`);
+console.log(`Work: ${work.address}`);
