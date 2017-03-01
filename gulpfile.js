a3a8798cef83a0f0f76fc35b889f4114acb28482
Jan Amoyo <jan.amoyo@siteminder.com>
2017-03-01 17:35:41 +1100

    use ts-npm-module 1.3.0 (postinstall)

--- gulpfile.js
+++ gulpfile.js
@@ -2,44 +2,8 @@ var gulp = require('gulp');
 var clean = require('gulp-clean');
 var ts = require('gulp-typescript');
 
-var _ = require('lodash');
 var merge = require('merge2');
 var runSequence = require('run-sequence');
-
-var tsModules = [
-    'ts-npm-module',
-    // 'other-ts-module-1',
-    // 'other-ts-module-2',
-];
-
-function moduleDir(module) {
-    return './node_modules/' + module;
-}
-
-function cleanDependency(module) {
-    return gulp.src(moduleDir(module) + '/dist/', { read: false }).pipe(clean());
-}
-
-function compileDependency(module) {
-    var tsProject = ts.createProject(moduleDir(module) + '/tsconfig.json');
-    var tsResult = gulp.src(moduleDir(module) + '/src/**/*.ts').pipe(tsProject());
-    return merge([
-        tsResult.dts.pipe(gulp.dest(moduleDir(module) + '/dist/')),
-        tsResult.js.pipe(gulp.dest(moduleDir(module) + '/dist/'))
-    ]);
-}
-
-gulp.task('clean:dependencies', function () {
-    return merge(_.map(tsModules, function (module) {
-        return cleanDependency(module);
-    }));
-});
-
-gulp.task('compile:dependencies', function () {
-    return merge(_.map(tsModules, function (module) {
-        return compileDependency(module);
-    }));
-});
 
 gulp.task('clean', function () {
     return gulp.src('dist/', { read: false }).pipe(clean());
@@ -55,11 +19,7 @@ gulp.task('compile', function () {
 });
 
 gulp.task('build', function (callback) {
-    runSequence(
-        ['clean:dependencies', 'clean'],
-        'compile:dependencies',
-        'compile',
-        callback);
+    runSequence('clean', 'compile', callback);
 });
 
 gulp.task('default', ['build']);
--- readme.md
+++ readme.md
@@ -5,42 +5,10 @@ Sample NodeJS project using a TypeScript module published to GitHub
 * or `npm install`
 
 ### Build
-* `gulp clean:dependencies`
-* `gulp compile:dependencies`
 * `gulp clean`
 * `gulp compile`
-* `gulp` (clean-dependencies + compile-dependencies + clean + compile)
+* `gulp` (clean + compile)
 
 ### Run
 * `yarn run main`
 * or `npm run main`
-
-### Including TypeScript Modules from GitHub
-* Module should include `./tsconfig.json`
-* Module source should be in `./src`
-* Module transpiles to `./dist`
-
-```
-$ yarn add git+ssh://git@github.com/<account>/<repository>.git#branch --save
-```
-or
-```
-$ npm install git+ssh://git@github.com/<account>/<repository>.git#branch --save
-```
-
-```
-// gulpfile.js
-var tsModules = [
-    'ts-npm-module',
-    'my-ts-module-1', // add ts module here
-    'my-ts-module-2', // add ts module here
-];
-```
-
-```
-$ gulp build
-```
-
-### Limitations
-* Docker cannot download from private GitHub repositories
-* `yarn install --production` removes generated code
